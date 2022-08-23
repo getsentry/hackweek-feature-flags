9cd3826f5f05ded6acd99870ba72e162fc367f1d
Armin Ronacher <armin.ronacher@active-4.com>
2022-08-23 11:20:11 +0200

    Check in test for rand.py

--- spec/rand.py
+++ spec/rand.py
@@ -18,7 +18,7 @@ class XorShift(object):
         #   (x[0] << 24) | (x[1] << 16) | (x[2] << 8) | (x[3])
         self.state[:] = struct.unpack(">IIII", bytes[:16])
 
-    def next(self):
+    def next_u32(self):
         t = self.state[3]
         s = self.state[0]
 
@@ -30,9 +30,30 @@ class XorShift(object):
         t ^= t >> 8
         self.state[0] = (t ^ s ^ (s >> 19)) & MASK
 
-        return self.state[0] / MASK
+        return self.state[0]
+
+    def next(self):
+        self.next_u32() / MASK
 
 
 def prandom(sticky_id):
     rand = XorShift(seed=sticky_id)
     return rand.next()
+
+
+def test():
+    rand = XorShift(seed="wohoo")
+    assert rand.next_u32() == 3709882355
+    assert rand.next_u32() == 3406141351
+    assert rand.next_u32() == 2220835615
+    assert rand.next_u32() == 1978561524
+    assert rand.next_u32() == 2006162129
+    assert rand.next_u32() == 1526862107
+    assert rand.next_u32() == 2715875971
+    assert rand.next_u32() == 3524055327
+    assert rand.next_u32() == 1313248726
+    assert rand.next_u32() == 1591659718
+
+
+if __name__ == '__main__':
+    test()
